c890f252eeaec72f0a65ff7b0515152baa19838a
thesilentcoder-ai <pawanparashar@zoho.com>
2025-08-15 21:29:43 -0700

    Create 001_binary_Search.py

---
+++ binary_Search/001_binary_Search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        L , R = 0 , len(nums)-1
+
+        while L <= R: 
+            M = (L+R) // 2
+            if nums[M] == target:
+                return M
+            elif nums[M] > target: 
+                R = M -1 
+            else: 
+                L = M + 1
+
+        return -1
